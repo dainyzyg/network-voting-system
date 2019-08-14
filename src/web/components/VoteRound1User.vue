246d1ea5dcaa98acc4f133006d2de6c2a4708a79
dainyzyg <470067721@qq.com>
2019-08-14 04:52:19 -0400

    管理页面

---
+++ src/web/components/VoteRound1User.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+Modal(:show='show')
+  .content
+    .tit 未投票人员({{todo.count}})：
+    .voter {{todo.text}}
+    .tit 已投票人员({{done.count}})：
+    .voter {{done.text}}
+  .btn-wrapper
+   .btn(@click="close") 关闭
+   .btn(@click="refesh") 刷新
+</template>
+
+<script>
+import Vue from 'vue'
+import Modal from '@/components/Modal.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Modal
+  },
+  props: ['show', 'Round1User'],
+  data() {
+    return {}
+  },
+  methods: {
+    close() {
+      this.$emit('update:show', false)
+    },
+    refesh() {
+      this.$emit('refesh')
+    }
+  },
+  computed: {
+    done() {
+      if (this.Round1User && this.Round1User.length > 0) {
+        let list = this.Round1User.filter(i => i.isVote).map(m => m.name)
+        return { text: list.join(','), count: list.length }
+      }
+      return { text: '', count: 0 }
+    },
+    todo() {
+      if (this.Round1User && this.Round1User.length > 0) {
+        let list = this.Round1User.filter(i => !i.isVote).map(m => m.name)
+        return { text: list.join(','), count: list.length }
+      }
+      return { text: '', count: 0 }
+    }
+  }
+}
+</script>
+<style scoped>
+.content {
+  color: slategray;
+  flex: 1;
+  padding: 20px;
+  overflow: auto;
+  -webkit-overflow-scrolling: touch;
+}
+.btn-wrapper {
+  flex: 0 0 50px;
+  overflow: hidden;
+  display: flex;
+  /* justify-content: space-between; */
+  align-items: center;
+}
+.btn {
+  flex: 1;
+  margin: 0 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* margin: 20px; */
+  padding: 5px;
+  height: 30px;
+  /* width: 120px; */
+  border-radius: 5px;
+  color: aliceblue;
+  background: #409eff;
+  cursor: pointer;
+}
+.tit {
+  margin-top: 20px;
+  color: #2e89dc;
+  font-weight: bold;
+}
+</style>
--- src/web/views/mobile/Management.vue
+++ src/web/views/mobile/Management.vue
@@ -1,39 +1,80 @@
 <template lang="pug">
   .app
     .result {{result}}
-    .btn(@click="openModal") 设置当前为第一轮投票
-    .btn(@click="openModal") 设置当前为第二轮投票
-    .btn(@click="openModal") 查看第一轮投票人数
+    .btn(@click="setRound(1)") 设置当前为第一轮投票
+    .btn(@click="setRound(2)") 设置当前为第二轮投票
+    .btn(@click="getRound1User") 查看第一轮投票人数
     .btn(@click="openModal") 查看第一轮投票结果
-    .btn(@click="openModal") 查看第二轮投票人数
+    .btn(@click="getRound2User") 查看第二轮投票人数
     .btn(@click="openModal") 查看第二轮投票结果
     .btn(@click="openModal") 重置投票数据
-    Modal(:show.sync="show")
+    VoteRound1User(@refesh="getRoundUser" :show.sync="showRound1User" :Round1User="Round1User")
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Modal from '@/components/Modal.vue'
+import VoteRound1User from '@/components/VoteRound1User.vue'
 
 export default Vue.extend({
   name: 'home',
   components: {
-    Modal
+    VoteRound1User
   },
   data() {
     return {
-      show: false,
-      result: ''
+      round: 1,
+      showRound1User: false,
+      result: '',
+      Round1User: []
     }
   },
   methods: {
     openModal() {
-      this.show = true
+      // this.show = true
+    },
+    async setRound(index) {
+      let r = await this.$axios.post('setRound', { round: index })
+      if (r.data.success) {
+        this.result = `设置成功，当前投票轮次为：${index}`
+      } else {
+        this.result = `设置失败`
+      }
+    },
+    getRound1User() {
+      this.round = 1
+      this.getRoundUser()
+    },
+    getRound2User() {
+      this.round = 2
+      this.getRoundUser()
+    },
+    async getRoundUser() {
+      this.Round1User = []
+      this.showRound1User = true
+      if (this.round == 1) {
+        console.log('getRound1User')
+        let r = await this.$axios.get('getRound1User')
+        this.Round1User = r.data
+      } else {
+        console.log('getRound2User')
+        let r = await this.$axios.get('getRound2User')
+        this.Round1User = r.data
+      }
     }
   }
 })
 </script>
 <style scoped>
+.result {
+  background: #a9e0ff;
+  color: #2e89dc;
+  /* font-size: 50px; */
+  border-radius: 5px;
+  height: 60px;
+  margin: 20px;
+  padding: 5px;
+  overflow: auto;
+}
 .btn {
   display: flex;
   justify-content: center;
